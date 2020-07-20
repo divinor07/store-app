017f6cf7e13cd6f3f707aaed3955ff1982571435
Divino Rodrigues <divino.sr@hotmail.com>
2020-07-19 21:03:41 -0300

    Creating LoginScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/screens/base/base_screen.dart';
+import 'package:store_app/screens/login/login_screen.dart';
 
 void main() async {
   runApp(MyApp());
@@ -12,10 +13,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Loja Virtual',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color.fromARGB(255, 4, 125, 141),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: BaseScreen(),
+      home: LoginScreen(),
     );
   }
 }
