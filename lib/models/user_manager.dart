f6daca485ba344f70f4e33cab7108de0b5e8b59e
Divino Rodrigues <divino.sr@hotmail.com>
2020-07-21 12:42:36 -0300

    Improving the login experience

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:store_app/helpers/firebase_errors.dart';
 import 'package:store_app/models/user.dart';
 
-class UserManager {
+class UserManager extends ChangeNotifier {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
+  bool loading = false;
+
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
+    setLoading(true);
+
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
@@ -15,5 +20,11 @@ class UserManager {
     } on PlatformException catch (e) {
       onFail(getErrorString(e.code));
     }
+    setLoading(false);
+  }
+
+  void setLoading(bool value) {
+    loading = value;
+    notifyListeners();
   }
 }
