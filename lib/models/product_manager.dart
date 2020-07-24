1216cbe2b2b0529caa93404f989eda18488f7427
Divino Rodrigues <divino.sr@hotmail.com>
2020-07-24 08:39:47 -0300

    Converting the document to a product

--- lib/models/product_manager.dart
+++ lib/models/product_manager.dart
@@ -1,18 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:store_app/models/product.dart';
 
-class ProductManager {
+class ProductManager extends ChangeNotifier {
   ProductManager() {
     _loadAllProducts();
   }
 
   final Firestore firestore = Firestore.instance;
 
+  List<Product> _allProducts = [];
+
   Future<void> _loadAllProducts() async {
     final QuerySnapshot snapProducts =
         await firestore.collection('products').getDocuments();
 
-    for (DocumentSnapshot doc in snapProducts.documents) {
-      print(doc.data);
-    }
+    _allProducts =
+        snapProducts.documents.map((d) => Product.fromDocument(d)).toList();
+
+    notifyListeners();
   }
 }
