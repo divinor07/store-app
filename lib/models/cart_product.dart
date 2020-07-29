12bc9121a99302cda4e41396752854e1926d1837
Divino Rodrigues <divino.sr@hotmail.com>
2020-07-29 16:34:13 -0300

    Changing the quantity in the cart

--- lib/models/cart_product.dart
+++ lib/models/cart_product.dart
@@ -49,4 +49,12 @@ class CartProduct {
   bool stackable(Product product) {
     return product.id == productId && product.selectedSize.name == size;
   }
+
+  void increment() {
+    quantity++;
+  }
+
+  void decrement() {
+    quantity--;
+  }
 }
