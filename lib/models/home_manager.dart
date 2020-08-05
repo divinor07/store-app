8b45a755203d33d4bf0c063380a3fd99a29c0969
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-04 23:38:00 -0300

    Fetching images from sections

--- lib/models/home_manager.dart
+++ lib/models/home_manager.dart
@@ -16,6 +16,7 @@ class HomeManager {
       for (final DocumentSnapshot document in snapshot.documents) {
         sections.add(Section.fromDocument(document));
       }
+      print(sections);
     });
   }
 }
