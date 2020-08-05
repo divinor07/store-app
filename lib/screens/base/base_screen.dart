bbf1468f8f06cfdb769e70f0ec34cf147ca9ab18
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-05 18:26:44 -0300

    Creating the floating bar

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store_app/common/custom_drawer/custom_drawer.dart';
 import 'package:store_app/models/page_manager.dart';
+import 'package:store_app/screens/home/home_screen.dart';
 import 'package:store_app/screens/products/products_screen.dart';
 
 class BaseScreen extends StatelessWidget {
@@ -15,12 +16,7 @@ class BaseScreen extends StatelessWidget {
         controller: pageController,
         physics: const NeverScrollableScrollPhysics(),
         children: <Widget>[
-          Scaffold(
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              title: const Text('Home'),
-            ),
-          ),
+          HomeScreen(),
           ProductsScreen(),
           Scaffold(
             drawer: CustomDrawer(),
