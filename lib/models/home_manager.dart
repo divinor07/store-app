e2e70e38cf88e74a6461a4f83bcf3dbb0b6ea853
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-11 15:04:01 -0300

    Displaying images in list

--- lib/models/home_manager.dart
+++ lib/models/home_manager.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:store_app/models/section.dart';
 
-class HomeManager {
+class HomeManager extends ChangeNotifier {
   HomeManager() {
     _loadSections();
   }
@@ -16,7 +17,7 @@ class HomeManager {
       for (final DocumentSnapshot document in snapshot.documents) {
         sections.add(Section.fromDocument(document));
       }
-      print(sections);
+      notifyListeners();
     });
   }
 }
