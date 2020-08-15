6c514d097b90b3ec47bc8cf46631a07720a090a4
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-15 18:30:18 -0300

    Custom form for images

---
+++ lib/screens/edit_product/components/images_form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:store_app/models/product.dart';
+
+class ImagesForm extends StatelessWidget {
+  const ImagesForm(this.product);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField<List<dynamic>>(
+      initialValue: product.images,
+      builder: (state) {},
+    );
+  }
+}
--- lib/screens/edit_product/edit_product_screen.dart
+++ lib/screens/edit_product/edit_product_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/models/product.dart';
+import 'package:store_app/screens/edit_product/components/images_form.dart';
 
 class EditProductScreen extends StatelessWidget {
+  const EditProductScreen(this.product);
+
+  final Product product;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Editar Anúncio'),
         centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ImagesForm(product),
+        ],
       ),
     );
   }
