f94aff82fcad9e6a6abd42cf056433cf4d33e994
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-16 02:12:57 -0300

    Displaying the images on the form

--- lib/screens/edit_product/components/images_form.dart
+++ lib/screens/edit_product/components/images_form.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:store_app/models/product.dart';
 
@@ -10,7 +13,40 @@ class ImagesForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return FormField<List<dynamic>>(
       initialValue: product.images,
-      builder: (state) {},
+      builder: (state) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Carousel(
+            images: state.value.map((image) {
+              return Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  if (image is String)
+                    Image.network(image, fit: BoxFit.cover)
+                  else
+                    Image.file(image as File, fit: BoxFit.cover),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: Icon(Icons.remove),
+                      color: Colors.red,
+                      onPressed: () {
+                        state.value.remove(image);
+                        state.didChange(state.value);
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }).toList(),
+            dotSize: 4,
+            dotSpacing: 15,
+            dotBgColor: Colors.transparent,
+            dotColor: Theme.of(context).primaryColor,
+            autoplay: false,
+          ),
+        );
+      },
     );
   }
 }
