9399f319749e3ca40e3b3394404cd194f9e3ce67
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-22 10:45:40 -0300

    Editing title and removing section

--- lib/screens/home/components/section_header.dart
+++ lib/screens/home/components/section_header.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_app/common/custom_icon_button.dart';
+import 'package:store_app/models/home_manager.dart';
 import 'package:store_app/models/section.dart';
 
 class SectionHeader extends StatelessWidget {
@@ -8,16 +11,45 @@ class SectionHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Text(
-        section.name ?? "Teste",
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w800,
-          fontSize: 18,
+    final homeManager = context.watch<HomeManager>();
+
+    if (homeManager.editing) {
+      return Row(
+        children: <Widget>[
+          Expanded(
+            child: TextFormField(
+              initialValue: section.name,
+              decoration: const InputDecoration(
+                  hintText: 'Título', isDense: true, border: InputBorder.none),
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 18,
+              ),
+              onChanged: (text) => section.name = text,
+            ),
+          ),
+          CustomIconButton(
+            iconData: Icons.remove,
+            color: Colors.white,
+            onTap: () {
+              homeManager.removeSection(section);
+            },
+          ),
+        ],
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Text(
+          section.name ?? "Banana",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w800,
+            fontSize: 18,
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
