bfc7c3f1f48b38c7d410913f2b221380e5455a2e
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-21 22:19:55 -0300

    Local product loading and updating indicator

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -35,6 +35,13 @@ class Product extends ChangeNotifier {
 
   List<dynamic> newImages;
 
+  bool _loading = false;
+  bool get loading => _loading;
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
   ItemSize _selectedSize;
   ItemSize get selectedSize => _selectedSize;
   set selectedSize(ItemSize value) {
@@ -75,6 +82,8 @@ class Product extends ChangeNotifier {
   }
 
   Future<void> save() async {
+    loading = true;
+
     final Map<String, dynamic> data = {
       'name': name,
       'description': description,
@@ -114,6 +123,10 @@ class Product extends ChangeNotifier {
     }
 
     await firestoreRef.updateData({'images': updateImages});
+
+    images = updateImages;
+
+    loading = false;
   }
 
   Product clone() {
