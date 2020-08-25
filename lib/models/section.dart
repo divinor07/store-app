69b7305adaac0115d801ee00c3adea4d8af7da5f
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-24 23:45:33 -0300

    Saving section images

--- lib/models/section.dart
+++ lib/models/section.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:store_app/models/section_item.dart';
+import 'package:uuid/uuid.dart';
 
 class Section extends ChangeNotifier {
   Section({this.id, this.name, this.type, this.items}) {
     items = items ?? [];
+    originalItems = List.from(items);
   }
 
   Section.fromDocument(DocumentSnapshot document) {
@@ -17,13 +22,16 @@ class Section extends ChangeNotifier {
   }
 
   final Firestore firestore = Firestore.instance;
+  final FirebaseStorage storage = FirebaseStorage.instance;
 
   DocumentReference get firestoreRef => firestore.document('home/$id');
+  StorageReference get storageRef => storage.ref().child('home/$id');
 
   String id;
   String name;
   String type;
   List<SectionItem> items;
+  List<SectionItem> originalItems;
 
   String _error;
   String get error => _error;
@@ -54,6 +62,33 @@ class Section extends ChangeNotifier {
     } else {
       await firestoreRef.updateData(data);
     }
+
+    for (final item in items) {
+      if (item.image is File) {
+        final StorageUploadTask task =
+            storageRef.child(Uuid().v1()).putFile(item.image as File);
+        final StorageTaskSnapshot snapshot = await task.onComplete;
+        final String url = await snapshot.ref.getDownloadURL() as String;
+        item.image = url;
+      }
+    }
+
+    for (final original in originalItems) {
+      if (!items.contains(original)) {
+        try {
+          final ref =
+              await storage.getReferenceFromUrl(original.image as String);
+          await ref.delete();
+          // ignore: empty_catches
+        } catch (e) {}
+      }
+    }
+
+    final Map<String, dynamic> itemsData = {
+      'items': items.map((e) => e.toMap()).toList()
+    };
+
+    await firestoreRef.updateData(itemsData);
   }
 
   bool valid() {
