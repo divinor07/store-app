3ff7486cfb68b4ac8983a8b6f1a8dc0693cde9ec
Divino Rodrigues <divino.sr@hotmail.com>
2020-08-25 15:45:22 -0300

    Calculating delivery rate

--- lib/models/cart_manager.dart
+++ lib/models/cart_manager.dart
@@ -15,6 +15,7 @@ class CartManager extends ChangeNotifier {
   Address address;
 
   num productsPrice = 0.0;
+  num deliveryPrice;
 
   final Firestore firestore = Firestore.instance;
 
@@ -117,10 +118,14 @@ class CartManager extends ChangeNotifier {
     }
   }
 
-  void setAddress(Address address) {
+  Future<void> setAddress(Address address) async {
     this.address = address;
 
-    calculateDelivery(address.lat, address.long);
+    if (await calculateDelivery(address.lat, address.long)) {
+      print('price $deliveryPrice');
+    } else {
+      return Future.error('Endereço fora do raio de entrega :(');
+    }
   }
 
   void removeAddress() {
@@ -128,12 +133,14 @@ class CartManager extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> calculateDelivery(double lat, double long) async {
+  Future<bool> calculateDelivery(double lat, double long) async {
     final DocumentSnapshot doc = await firestore.document('aux/delivery').get();
 
     final latStore = doc.data['lat'] as double;
     final longStore = doc.data['long'] as double;
 
+    final base = doc.data['base'] as num;
+    final km = doc.data['km'] as num;
     final maxkm = doc.data['maxkm'] as num;
 
     double dis =
@@ -141,6 +148,13 @@ class CartManager extends ChangeNotifier {
 
     dis /= 1000.0;
 
-    print('Distance $dis');
+    debugPrint('Distance $dis');
+
+    if (dis > maxkm) {
+      return false;
+    }
+
+    deliveryPrice = base + dis * km;
+    return true;
   }
 }
