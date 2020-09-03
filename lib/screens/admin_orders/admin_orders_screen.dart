21a688e0363bf94014a0c88ecfe6e64fb21f64e5
Divino Rodrigues <divino.sr@hotmail.com>
2020-09-03 12:58:41 -0300

    Displaying the status panel

--- lib/screens/admin_orders/admin_orders_screen.dart
+++ lib/screens/admin_orders/admin_orders_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:store_app/common/custom_drawer/custom_drawer.dart';
 import 'package:store_app/common/custom_icon_button.dart';
 import 'package:store_app/common/empty_card.dart';
 import 'package:store_app/common/order/order_tile.dart';
 import 'package:store_app/models/admin_orders_manager.dart';
+import 'package:store_app/models/order.dart';
 
 class AdminOrdersScreen extends StatelessWidget {
+  final PanelController panelController = PanelController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,51 +23,96 @@ class AdminOrdersScreen extends StatelessWidget {
         builder: (_, ordersManager, __) {
           final filteredOrders = ordersManager.filteredOrders;
 
-          return Column(
-            children: <Widget>[
-              if (ordersManager.userFilter != null)
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 2),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Text(
-                          'Pedidos de ${ordersManager.userFilter.name}',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w800,
-                            color: Colors.white,
+          return SlidingUpPanel(
+            controller: panelController,
+            body: Column(
+              children: <Widget>[
+                if (ordersManager.userFilter != null)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 2),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            'Pedidos de ${ordersManager.userFilter.name}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
+                        CustomIconButton(
+                          iconData: Icons.close,
+                          color: Colors.white,
+                          onTap: () {
+                            ordersManager.setUserFilter(null);
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                if (filteredOrders.isEmpty)
+                  Expanded(
+                    child: EmptyCard(
+                      title: 'Nenhuma venda realizada!',
+                      iconData: Icons.border_clear,
+                    ),
+                  )
+                else
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: filteredOrders.length,
+                        itemBuilder: (_, index) {
+                          return OrderTile(
+                            filteredOrders[index],
+                            showControls: true,
+                          );
+                        }),
+                  )
+              ],
+            ),
+            minHeight: 40,
+            maxHeight: 250,
+            panel: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    if (panelController.isPanelClosed) {
+                      panelController.open();
+                    } else {
+                      panelController.close();
+                    }
+                  },
+                  child: Container(
+                    height: 40,
+                    color: Colors.white,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Filtros',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 16,
                       ),
-                      CustomIconButton(
-                        iconData: Icons.close,
-                        color: Colors.white,
-                        onTap: () {
-                          ordersManager.setUserFilter(null);
-                        },
-                      )
-                    ],
+                    ),
                   ),
                 ),
-              if (filteredOrders.isEmpty)
-                const Expanded(
-                  child: EmptyCard(
-                    title: 'Nenhuma venda realizada!',
-                    iconData: Icons.border_clear,
-                  ),
-                )
-              else
                 Expanded(
-                  child: ListView.builder(
-                      itemCount: filteredOrders.length,
-                      itemBuilder: (_, index) {
-                        return OrderTile(
-                          filteredOrders[index],
-                          showControls: true,
-                        );
-                      }),
-                )
-            ],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: Status.values.map((s) {
+                      return CheckboxListTile(
+                        title: Text(Order.getStatusText(s)),
+                        dense: true,
+                        value: true,
+                        onChanged: (v) {},
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
           );
         },
       ),
